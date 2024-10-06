b2bd07b4986b45649556b66b95a3b9be2310595a
hwangjunseong <junseong5832@gmail.com>
2024-10-06 12:31:59 +0900

    compare crypto , bcrypt

---
+++ demo-node-js/bcrypt-demo.js
@@ -0,0 +1,11 @@
+const bcrypt = require("bcryptjs");
+
+const password = "1111";
+const bar = async () => {
+  const hashedPassword = await bcrypt.hash(password, 12);
+  console.log(hashedPassword);
+};
+bar();
+//$2a$12$m61XNnjjr6.57jt10M9cX.tJAUm85y8ZRP2rKX0rj6sLs5ca.WqEe
+//$2a$12$LB4s2CoxTckAVBrZfyfNjeAKPr3s4JPoz.3ootN1UHKLef8IorVhK
+//매번 다르게나옴
